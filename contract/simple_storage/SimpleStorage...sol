5e4f59110ae2ed447130d7a07f2739eb2dad27af
adel <adeelguitoun@gmail.com>
2023-04-23 21:48:58 +0100

    Add struct and arrays of People type

--- contract/simple_storage/SimpleStorage...sol
+++ contract/simple_storage/SimpleStorage...sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     // Solidity Basics Types: boolean, uint, int, address, bytes
     uint256 favoriteNumber;
+    People[] public people;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
@@ -12,5 +18,10 @@ contract SimpleStorage {
     // View: Doesn't cost gas!!, Doesn't change the variable states.
     function retrieve() public view returns (uint256) {
         return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory person = People(_favoriteNumber, _name);
+        people.push(person);
     }
 }
