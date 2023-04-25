53ef051c1367bc276bcd7d453bf1725f229e27cf
adel <adeelguitoun@gmail.com>
2023-04-25 18:46:45 +0100

    Implement withdraw

--- contract/fundme/FundMe.sol
+++ contract/fundme/FundMe.sol
@@ -6,10 +6,14 @@ import "./PriceConverter.sol";
 contract FundMe {
     using PriceConverter for uint256;
 
+    // min fund allowed
     uint256 public minUsd = 50 * 1e18;
+
+    // Funders, and the mapping to the amount funded by funders.
     address[] funders;
     mapping(address => uint256) public addressToAmountfunded;
 
+    // Sending value ==> amount of funding by who ever is calling the fund().
     function fund() public payable {
         require(
             msg.value.PriceConverter() >= minUsd,
@@ -17,5 +21,26 @@ contract FundMe {
         );
         funders.push(msg.sender);
         addressToAmountfunded[msg.sender] = msg.value;
+    }
+
+    // withdraw method
+    function withdraw() public {
+        // Reset the mapping
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountfunded[funder] = 0;
+        }
+
+        // Reset the funders array
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Withdra failed!");
     }
 }
