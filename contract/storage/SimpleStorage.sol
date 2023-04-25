706160c096c97e5600e4675f19d5b31a8bc438c6
adel <adeelguitoun@gmail.com>
2023-04-25 11:31:22 +0100

    Update store memeber to be virtual

--- contract/storage/SimpleStorage.sol
+++ contract/storage/SimpleStorage.sol
@@ -11,7 +11,7 @@ contract SimpleStorage {
     // Mapping Types
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public virtual {
         favoriteNumber = _favoriteNumber;
     }
 
