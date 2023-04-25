5553fb00a4f9a8c0fa08236586e9b2f41d0b9b75
adel <adeelguitoun@gmail.com>
2023-04-25 15:59:23 +0100

    Implement the getPrice function

--- contract/fundme/FundMe.sol
+++ contract/fundme/FundMe.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
+    uint256 public minUsd = 50;
+
     function fund() public payable {
-        require(msg.value > 1e18, "Didn't send enough eth!");
+        require(msg.value > minUsd, "Didn't send enough eth!");
+    }
+
+    function getPrice() public view returns (int256) {
+        address sepoliaNet = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(sepoliaNet);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return price;
     }
 }
