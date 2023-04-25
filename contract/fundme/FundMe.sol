a3f6f53090333b6b770e7fc9ca8a1e513f0c40ab
adel <adeelguitoun@gmail.com>
2023-04-25 16:02:25 +0100

    Get the exact floating point price

--- contract/fundme/FundMe.sol
+++ contract/fundme/FundMe.sol
@@ -10,10 +10,10 @@ contract FundMe {
         require(msg.value > minUsd, "Didn't send enough eth!");
     }
 
-    function getPrice() public view returns (int256) {
+    function getPrice() public view returns (uint256) {
         address sepoliaNet = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
         AggregatorV3Interface priceFeed = AggregatorV3Interface(sepoliaNet);
         (, int256 price, , , ) = priceFeed.latestRoundData();
-        return price;
+        return uint256(price * 1e10);
     }
 }
