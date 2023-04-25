9dc3400096aa0d67cc2c6a29ec37f8a286af4c30
adel <adeelguitoun@gmail.com>
2023-04-25 17:17:55 +0100

    Collect funding, and map founders with amount funded

--- contract/fundme/FundMe.sol
+++ contract/fundme/FundMe.sol
@@ -5,12 +5,16 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
     uint256 public minUsd = 50 * 1e18;
+    address[] funders;
+    mapping(address => uint256) public addressToAmountfunded;
 
     function fund() public payable {
         require(
             getConversionRate(msg.value) > minUsd,
             "Didn't send enough eth!"
         );
+        funders.push(msg.sender);
+        addressToAmountfunded[msg.sender] = msg.value;
     }
 
     function getPrice() internal view returns (uint256) {
