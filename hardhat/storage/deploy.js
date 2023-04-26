1a170ea9bc78f9c70525005be60568e1989290dc
adel <adeelguitoun@gmail.com>
2023-04-26 10:44:23 +0100

    Create and invoke main()

---
+++ hardhat/storage/deploy.js
@@ -0,0 +1,10 @@
+// Deploy smart contract
+const main = async () => {};
+
+// Invoke main()
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
