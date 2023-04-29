f0e1818a00e7d86b4fe02c848a00b8631dd31046
adel <adeelguitoun@gmail.com>
2023-04-29 10:52:44 +0100

    Add simpleStorage contract

---
+++ hardhat/contracts/SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    // Solidity Basics Types: boolean, uint, int, address, bytes
+    uint256 favoriteNumber;
+    People[] public people;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Mapping Types
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // View: Doesn't cost gas!!, Doesn't change the variable states.
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory person = People(_favoriteNumber, _name);
+        people.push(person);
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
