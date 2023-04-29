3184e5882424f9c2a8e37a8b314697e0d698b7c7
adel <adeelguitoun@gmail.com>
2023-04-29 14:38:47 +0100

    verify contract

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,9 +1,11 @@
 require('@nomicfoundation/hardhat-toolbox');
+
 require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC = process.env.SEPOLIA_RPC;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETH_SCAN_API_KEY = process.env.ETH_SCAN_API_KEY;
 
 module.exports = {
   networks: {
@@ -12,6 +14,9 @@ module.exports = {
       accounts: [`${PRIVATE_KEY}`],
       chainId: 11155111,
     },
+  },
+  etherscan: {
+    apiKey: `BVR9QKVUT1T44PIZV4IHWA7B5IU8VY6F37`,
   },
   solidity: '0.8.18',
 };
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,11 +1,32 @@
-const { ethers } = require('hardhat');
+const { ethers, run, network } = require('hardhat');
+require('dotenv').config();
 
 // Deploy contract
 async function main() {
   const simpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
   console.log('Deploying contract please wait...');
   const simpleStorage = await simpleStorageFactory.deploy();
-  console.log(simpleStorage.address);
+  console.log('Verifying Contract please wait...');
+  if (network.config.chainId === 11155111 && process.env.ETH_SCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6);
+    verify(simpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log('Verify Contract...');
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArgsParams: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes('already verified')) {
+      console.log('Already verified');
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 // main
