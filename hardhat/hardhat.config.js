5a445ad64b536f67dfa38c9dd3eb40f0830f8da9
adel <adeelguitoun@gmail.com>
2023-04-29 13:38:51 +0100

    Add sepolia network

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC = process.env.SEPOLIA_RPC;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `${SEPOLIA_RPC}`,
+      accounts: [`${PRIVATE_KEY}`],
+      chainId: 11155111,
+    },
+  },
+  solidity: '0.8.18',
 };
