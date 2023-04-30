2b0b62255d7df2cd62f5fe9a1d044fa7b458fafa
adel <adeelguitoun@gmail.com>
2023-04-30 11:19:22 +0100

    Interact with simplestorage contract

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -6,15 +6,29 @@ async function main() {
   const simpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
   console.log('Deploying contract please wait...');
   const simpleStorage = await simpleStorageFactory.deploy();
-  console.log('Verifying Contract please wait...');
-  if (network.config.chainId === 11155111 && process.env.ETH_SCAN_API_KEY) {
+  // Verify the smart contract
+  if (
+    network.config.chainId === 11155111 &&
+    `${process.env.ETH_SCAN_API_KEY}`
+  ) {
     await simpleStorage.deployTransaction.wait(6);
     verify(simpleStorage.address, []);
   }
+
+  // Interact with the smart contract
+  // Retrieve current favoriteNumber value
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Favorite Number is: ${currentValue}`);
+
+  // Update Current value
+  const txResponse = await simpleStorage.store('7');
+  await txResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated Favorite Number is: ${updatedValue}`);
 }
 
 async function verify(contractAddress, args) {
-  console.log('Verify Contract...');
+  console.log('Verifying Contract please wait...');
   try {
     await run('verify:verify', {
       address: contractAddress,
