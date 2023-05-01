3c40ec68051c3b58acbb2830ab283607d43d8ace
adel <adeelguitoun@gmail.com>
2023-05-01 09:02:05 +0100

    Implement test for retrieve method

---
+++ hardhat/test/test-deploy.js
@@ -0,0 +1,16 @@
+const { expect, assert } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('SimpleStorage', () => {
+  let simpleStorageFactory, simpleStorage;
+  beforeEach(async () => {
+    simpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
+    simpleStorage = await simpleStorageFactory.deploy();
+  });
+
+  it('Should start with favorite number of 0', async () => {
+    const currentValue = await simpleStorage.retrieve();
+    const expectedvalue = '0';
+    assert.equal(currentValue.toString(), expectedvalue);
+  });
+});
