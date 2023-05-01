219d20e307d9f0e59c7f468ef52d94d8e510a926
adel <adeelguitoun@gmail.com>
2023-05-01 09:24:35 +0100

    Config gas reporter

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,5 +1,6 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('./tasks/block-number');
+require('hardhat-gas-reporter');
 
 require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -7,6 +8,7 @@ require('dotenv').config();
 const SEPOLIA_RPC = process.env.SEPOLIA_RPC;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETH_SCAN_API_KEY = process.env.ETH_SCAN_API_KEY;
+const COINMARKETCAP_KEY = process.env.COINMARKETCAP_KEY;
 
 module.exports = {
   networks: {
@@ -15,9 +17,20 @@ module.exports = {
       accounts: [`${PRIVATE_KEY}`],
       chainId: 11155111,
     },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      chainId: 31337,
+    },
   },
   etherscan: {
     apiKey: `${ETH_SCAN_API_KEY}`,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.md',
+    noColors: true,
+    currency: 'USD',
+    coinmarketcap: `${COINMARKETCAP_KEY}`,
   },
   solidity: '0.8.18',
 };
